e5564fc757213460a709fb0ad1ad52f3b536ff86
Gwion Tangi <lil.smiss7@gmail.com>
2023-03-08 20:14:12 +0100

    added tabbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <van-tabbar v-model="active">
+    <van-tabbar-item name="home" to="/" icon="home-o"></van-tabbar-item>
+    <van-tabbar-item name="like" to="/like" icon="like-o"></van-tabbar-item>
+    <van-tabbar-item name="add" to="/add" icon="add-square"></van-tabbar-item>
+    <van-tabbar-item name="notification" to="/notification" icon="diamond-o"></van-tabbar-item>
+    <van-tabbar-item name="user" to="/user" icon="user-o"></van-tabbar-item>
+    </van-tabbar>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const active = ref("home");
+    return { active };
+  },
+};
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import { Tabbar, TabbarItem } from 'vant';
+import 'vant/lib/index.css';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(Tabbar);
+app.use(TabbarItem);
+app.use(router);
+app.mount('#app');
